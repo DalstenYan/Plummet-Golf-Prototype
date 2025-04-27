f075b8da5bb85d1f1daa66bb916cf1fc63a9acf9
Connor Sharp <connorsharp2003@gmail.com>
2025-04-27 13:16:08 -0500

    Fixed fade out

--- Assets/Scripts/PanelFade.cs
+++ Assets/Scripts/PanelFade.cs
@@ -2,42 +2,49 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class PanelFade : MonoBehaviour
 {
     private bool faded = false;
-    [SerializeField] private float timer;
-    [SerializeField] private float duration;
+    [SerializeField] private float timer = 3f; // Countdown before fade
+    [SerializeField] private float duration = 1f; // Duration of fade
     private bool itHappened = false;
 
     CanvasGroup canGroup;
-
 
     private void Start()
     {
         canGroup = GetComponent<CanvasGroup>();
         Debug.Log("still working");
-        
-        while(timer > 0)
-        {
-            timer -= Time.deltaTime;
-        }
-        fade();
 
+        
+        StartCoroutine(StartFadeAfterDelay());
     }
+
+    private IEnumerator StartFadeAfterDelay()
+    {
+    
+        float countdown = timer;
+        while (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
+            yield return null;
+        }
+
+        fade();
+    }
+
     public void fade()
     {
-
         StartCoroutine(DoFade(canGroup.alpha, faded ? 1 : 0));
-
         faded = !faded;
-
     }
 
     public IEnumerator DoFade(float start, float end)
     {
         float counter = 0.0f;
 
-        while(counter < duration)
+        while (counter < duration)
         {
             counter += Time.deltaTime;
             canGroup.alpha = Mathf.Lerp(start, end, counter / duration);
@@ -49,7 +56,5 @@ public class PanelFade : MonoBehaviour
         {
             itHappened = true;
         }
-
-
     }
 }
